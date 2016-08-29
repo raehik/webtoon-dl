ee6a5f9caee62d4e22b011ced76718acded80f2e
Ben Orchard <thefirstmuffinman@gmail.com>
2016-08-29 14:38:33 +0100

    initial commit: +current scripts & res

---
+++ rss-link-dl.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+#
+# Short description of the program/script's operation/function.
+#
+
+import sys
+import os
+import argparse
+from urllib import request
+from urllib.error import URLError
+import xml.etree.ElementTree as eltree
+from urllib import parse
+import re
+import subprocess
+import time
+
+FILENAME = sys.argv[0]
+
+link_dler = "./webtoon-dl.py"
+cache_file = os.environ["HOME"] + "/.cache/rss-link-dl"
+
+
+
+"""Argparse override to print usage to stderr on argument error."""
+class ArgumentParserUsage(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write("error: %s\n" % message)
+        self.print_help(sys.stderr)
+        sys.exit(2)
+
+"""Print usage and exit depending on given exit code."""
+def usage(exit_code):
+    if exit_code == 0:
+        pipe = sys.stdout
+    else:
+        # if argument was non-zero, print to STDERR instead
+        pipe = sys.stderr
+
+    parser.print_help(pipe)
+    sys.exit(exit_code)
+
+"""Log a message to a specific pipe (defaulting to stdout)."""
+def log_message(message, pipe=sys.stdout):
+    print("[{}] rss-link-dl: ".format(time.strftime("%F %T")) + message, file=pipe)
+
+"""If verbose, log an event."""
+def log(message):
+    if not args.verbose:
+        return
+    log_message(message)
+
+"""Log an error. If given a 2nd argument, exit using that error code."""
+def error(message, exit_code=None):
+    log_message("error: " + message, sys.stderr)
+    if exit_code:
+        sys.exit(exit_code)
+
+
+
+parser = ArgumentParserUsage(description="Description of the program's function (identical if you'd like).")
+
+# add arguments
+parser.add_argument("-v", "--verbose", action="store_true", help="be verbose")
+parser.add_argument("-r", "--rss", help="URL to comic RSS feed")
+
+# parse arguments
+args = parser.parse_args()
+
+# force verbosity for now
+args.verbose = True
+
+
+links = {}
+while True:
+    log("starting check...")
+    got_rss = False
+    while not got_rss:
+        try:
+            with request.urlopen(args.rss) as page:
+                data = page.read()
+            got_rss = True
+        except:
+            log_message("connection error, retrying in 12 hours")
+            time.sleep(60*60*12)
+
+    feed = eltree.fromstring(data)
+    for item in feed.iter("item"):
+        title = item.find("title").text
+        with open(cache_file, "r") as f:
+            seen_links = f.read().split("\n")
+        if title in seen_links:
+            log("title cached, not downloading: " + title)
+        else:
+            log("downloading title: " + title)
+            link = item.find("link").text
+
+            # get episode number (to use as directory)
+            link_parsed = parse.urlparse(link)
+            episode_num = int(parse.parse_qs(link_parsed.query)["episode_no"][0])
+
+            # fix episode number (theirs is always 1 higher?)
+            episode_num -= 1
+
+            # add to links dict
+            links[episode_num] = link
+
+            log("adding title to cache file")
+            with open(cache_file, "a") as f:
+                f.write(title + "\n")
+
+    # all links downloaded: process them
+    if len(links) > 0:
+        log("processing links with {}".format(link_dler))
+        for title, link in links.items():
+            subprocess.call([link_dler, "-d", str(title), link])
+    else:
+        log("No links to download")
+    log("check finished, waiting 1 week")
+    time.sleep(60*60*24*7)
---
+++ webtoon-dl.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+#
+# Download all images from a Naver Webtoon comic episode.
+#
+
+import sys
+import argparse
+import os
+from bs4 import BeautifulSoup
+from urllib import request
+import shutil
+
+FILENAME = sys.argv[0]
+
+
+
+"""Argparse override to print usage to stderr on argument error."""
+class ArgumentParserUsage(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write("error: %s\n" % message)
+        self.print_help(sys.stderr)
+        sys.exit(2)
+
+"""Print usage and exit depending on given exit code."""
+def usage(exit_code):
+    if exit_code == 0:
+        pipe = sys.stdout
+    else:
+        # if argument was non-zero, print to STDERR instead
+        pipe = sys.stderr
+
+    parser.print_help(pipe)
+    sys.exit(exit_code)
+
+"""Log a message to a specific pipe (defaulting to stdout)."""
+def log_message(message, pipe=sys.stdout):
+    print(FILENAME + ": " + message, file=pipe)
+
+"""If verbose, log an event."""
+def log(message):
+    if not args.verbose:
+        return
+    log_message(message)
+
+"""Log an error. If given a 2nd argument, exit using that error code."""
+def error(message, exit_code=None):
+    log_message("error: " + message, sys.stderr)
+    if exit_code:
+        sys.exit(exit_code)
+
+
+
+parser = ArgumentParserUsage(description="Download all images from a Naver Webtoon comic episode.")
+
+# add arguments
+parser.add_argument("-v", "--verbose", action="store_true", help="be verbose")
+parser.add_argument("-d", "--dir", default=".",
+                    help="directory to store downloaded images in (default: .)")
+parser.add_argument("url", metavar="URL", help="Webtoon comic URL")
+
+# parse arguments
+args = parser.parse_args()
+
+# force verbosity for now
+args.verbose = True
+
+
+img_referer = 'http://www.webtoons.com'
+
+def get_image_urls(page):
+    """Retrieve all image URLs to download."""
+    img_dl_urls = []
+
+    log("Downloading page {}".format(page))
+    req = request.urlopen(page).read()
+    soup = BeautifulSoup(req, "lxml")
+
+    imgs = soup.find_all(class_="_images")
+    for img in imgs:
+        # get image URL, remove the lower quality bit GET
+        img_url = img["data-url"].replace("?type=q90", "")
+        img_dl_urls.append(img_url)
+
+    return img_dl_urls
+
+def download_images(urls, outdir):
+    """Download each image in urls to existing directory outdir."""
+    referer_header = { "Referer": img_referer }
+    count = 0
+    for url in urls:
+        log("Downloading image {} at {}".format(count, url))
+        count += 1
+        req = request.Request(url, headers=referer_header)
+        with request.urlopen(req) as response, open("{}/{:03}.jpg".format(outdir, count), "wb") as outfile:
+            shutil.copyfileobj(response, outfile)
+
+
+if os.path.exists(args.dir):
+    if not os.path.isdir(args.dir):
+        error("-d arg is not a directory: {}".format(args.dir), 1)
+else:
+    os.mkdir(args.dir)
+
+img_urls = get_image_urls(args.url)
+download_images(img_urls, args.dir)

f8ab84da3b37d2492d705eea8fb70b004ab9139f
Ben Orchard <thefirstmuffinman@gmail.com>
2017-01-21 15:16:25 +0000

    rss-link-dl: rm scheduling; +dir arg (for link_dler); formatting

--- rss-link-dl.py
+++ rss-link-dl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Short description of the program/script's operation/function.
+# Retrieve comic links from a LINE Webtoon comic RSS feed.
 #
 
 import sys
@@ -16,10 +16,8 @@ import time
 
 FILENAME = sys.argv[0]
 
-link_dler = "./webtoon-dl.py"
+link_dler = "webtoon-dl.py"
 cache_file = os.environ["HOME"] + "/.cache/rss-link-dl"
-
-
 
 """Argparse override to print usage to stderr on argument error."""
 class ArgumentParserUsage(argparse.ArgumentParser):
@@ -41,7 +39,7 @@ def usage(exit_code):
 
 """Log a message to a specific pipe (defaulting to stdout)."""
 def log_message(message, pipe=sys.stdout):
-    print("[{}] rss-link-dl: ".format(time.strftime("%F %T")) + message, file=pipe)
+    print("{}: {}".format(FILENAME, message), file=pipe)
 
 """If verbose, log an event."""
 def log(message):
@@ -57,63 +55,52 @@ def error(message, exit_code=None):
 
 
 
-parser = ArgumentParserUsage(description="Description of the program's function (identical if you'd like).")
-
-# add arguments
+parser = ArgumentParserUsage(description="Retrieve comic links from a LINE Webtoon comic RSS feed.")
 parser.add_argument("-v", "--verbose", action="store_true", help="be verbose")
 parser.add_argument("rss", metavar="RSS", help="URL to comic RSS feed")
-
-# parse arguments
+parser.add_argument("-d", "--dir", action="store", default=".", help="base directory name to store episodes in (default: .)")
 args = parser.parse_args()
 
 # force verbosity for now
 args.verbose = True
 
+try:
+    with request.urlopen(args.rss) as page:
+        data = page.read()
+    got_rss = True
+except:
+    error("connection error", 1)
 
 links = {}
-while True:
-    log("starting check...")
-    got_rss = False
-    while not got_rss:
-        try:
-            with request.urlopen(args.rss) as page:
-                data = page.read()
-            got_rss = True
-        except:
-            log_message("connection error, retrying in 12 hours")
-            time.sleep(60*60*12)
-
-    feed = eltree.fromstring(data)
-    for item in feed.iter("item"):
-        title = item.find("title").text
-        with open(cache_file, "r") as f:
-            seen_links = f.read().split("\n")
-        if title in seen_links:
-            log("title cached, not downloading: " + title)
-        else:
-            log("downloading title: " + title)
-            link = item.find("link").text
-
-            # get episode number (to use as directory)
-            link_parsed = parse.urlparse(link)
-            episode_num = int(parse.parse_qs(link_parsed.query)["episode_no"][0])
-
-            # fix episode number (theirs is always 1 higher?)
-            episode_num -= 1
-
-            # add to links dict
-            links[episode_num] = link
-
-            log("adding title to cache file")
-            with open(cache_file, "a") as f:
-                f.write(title + "\n")
-
-    # all links downloaded: process them
-    if len(links) > 0:
-        log("processing links with {}".format(link_dler))
-        for title, link in links.items():
-            subprocess.call([link_dler, "-d", str(title), link])
+feed = eltree.fromstring(data)
+for item in feed.iter("item"):
+    title = item.find("title").text
+    with open(cache_file, "r") as f:
+        seen_links = f.read().split("\n")
+    if title in seen_links:
+        log("title cached, not downloading: " + title)
     else:
-        log("No links to download")
-    log("check finished, waiting 1 week")
-    time.sleep(60*60*24*7)
+        log("downloading title: " + title)
+        link = item.find("link").text
+
+        # get episode number (to use as directory)
+        link_parsed = parse.urlparse(link)
+        episode_num = int(parse.parse_qs(link_parsed.query)["episode_no"][0])
+
+        # fix episode number (theirs is always 1 higher?)
+        episode_num -= 1
+
+        # add to links dict
+        links[episode_num] = link
+
+        log("adding title to cache file")
+        with open(cache_file, "a") as f:
+            f.write(title + "\n")
+
+# all links downloaded: process them
+if len(links) > 0:
+    log("processing links with {}".format(link_dler))
+    for title, link in links.items():
+        subprocess.call([link_dler, "-d", "{}/{}".format(args.dir, str(title)), link])
+else:
+    log("no links to download")
